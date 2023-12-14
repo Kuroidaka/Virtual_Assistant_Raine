2ccbf651c5f3bc9e850be4f8f9b8265247eb422b
Canh <pcanhgm@gmail.com>
2023-12-14 21:52:26 +0700

    chore: config openai

--- config/openAI/index.js
+++ config/openAI/index.js
@@ -1,7 +1,7 @@
 const OpenAIApi = require('openai');    
 
 const openai = new OpenAIApi({
-    key: process.env.OPENAI_API_KEY,
+    apiKey: process.env.OPENAI_API_KEY,
 });
 
 module.exports = openai
