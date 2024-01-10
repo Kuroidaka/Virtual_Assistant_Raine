be195417405994f47aea5442b80ab177c8fd8bf6
Canh <pcanhgm@gmail.com>
2024-01-10 22:56:47 +0700

    feat: update tool checking only img file on discord

--- src/discord/eventHandler/detectUserSendFile.js
+++ src/discord/eventHandler/detectUserSendFile.js
@@ -5,23 +5,38 @@ module.exports = (dependencies) => {
         redisUseCase: { addToConversation }
     } } = dependencies;
 
+    const imgExtension = ['.jpg', '.jpeg', '.png', '.gif', '.bmp', '.tiff', '.svg', '.webp', '.heic', '.heif']
+
     const execute = ({interaction}) => {
         if(interaction.attachments.size > 0) {
             let prepareKey = interaction.channelId
-
+            let isValidSendImgFile = false
             // user attached files
             let filePrompt = "This is the file user attached: \n"				
             for (const [key, value] of interaction.attachments) {
-                filePrompt += `\tFile[${key}] Link: ${value.url}\n`
+                let fileName = value.name;
+                let lastDotPosition = fileName.lastIndexOf(".");
+                let fileExtension = fileName.substring(lastDotPosition + 1);
+                let fileURL = value.url
+
+                // check if file is image
+                let isImgFile = imgExtension.indexOf(`.${fileExtension}`) !== -1
+                if(isImgFile) {
+                    filePrompt += `File[${key}]\n\t- Type:${fileExtension}\n\t- Link: ${fileURL}\n`
+                    isValidSendImgFile = true
+                }
             }
-            console.log(filePrompt)
-            // add file prompt into redis
-            const data = {
-                role: "user",
-                content: filePrompt,
-                prepareKey: prepareKey
+
+            if(isValidSendImgFile) {
+                console.log(filePrompt)
+                // add file prompt into redis
+                const data = {
+                    role: "user",
+                    content: filePrompt,
+                    prepareKey: prepareKey
+                }
+                addToConversation(dependencies).execute(data)
             }
-            addToConversation(dependencies).execute(data)
         }
     }
 
